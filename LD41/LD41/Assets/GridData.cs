f86e974dc8ea5b1e92d6d4be29f7bde57994fc72
lslot <lfh.slot@gmail.com>
2018-04-16 23:46:10 +0200

    Skeleton for GridData, GridObject and Agent

---
+++ LD41/LD41/Assets/GridData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridData : MonoBehaviour {
+
+	const int gridSizeX = 4;
+	const int gridSizeY = 4;
+
+	// Use this for initialization
+	void Start () {
+		Tile[,] tileData = new Tile[gridSizeX, gridSizeY];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class Tile {
+	GridObject inhabitant;
+	public void SetInhabitant(GridObject inhabitant) {
+		this.inhabitant = inhabitant;
+	}
+
+	public GridObject GetInhabitant() {
+		return inhabitant;
+	}
+	
+	public Tile() {
+	
+	}
+}
