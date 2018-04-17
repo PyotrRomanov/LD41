30929c8ff63ae2edc80e569481901f4d0d28f262
DESKTOP-OG3SE7I\Bor <b.s.greve@students.uu.nl>
2018-04-17 22:36:21 +0200

    Added screen relative unity buttons

--- LD41/LD41/Assets/WorldManager.cs
+++ LD41/LD41/Assets/WorldManager.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WorldManager : MonoBehaviour {
 
+    public GameObject buttonPf;
+    public GameObject canvasPf;
+    const int buttonCount = 4;
+
 	void Start () {
 		GridWorld world1 = new GridWorld(4, 4);
-	}
+        GameObject canvas = Instantiate(canvasPf);
+        
+        for (int i = 0; i < buttonCount; i++) {
+            GameObject firstButton = Instantiate(buttonPf);
+            firstButton.transform.SetParent(canvas.transform, false);
+            firstButton.transform.localPosition = new Vector3(-(Screen.width / 2) + (firstButton.GetComponent<RectTransform>().sizeDelta.x / 2) + firstButton.GetComponent<RectTransform>().sizeDelta.x * i, -(Screen.height / 2) + (firstButton.GetComponent<RectTransform>().sizeDelta.y / 2), 0);
+
+        }
+
+    }
 }
